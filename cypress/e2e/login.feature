bb2ae703ab377a2730e85fb8bef99245878ae9c8
Natália Dias Almeida <natalia_2dias@hotmail.com>
2023-05-18 11:14:42 -0300

    feat: add login scenario

--- cypress/e2e/login.feature
+++ cypress/e2e/login.feature
@@ -1,12 +1,20 @@
 Feature: Login
+  Rule: 
+    Scenario: Non Existent User login
+      Given I visit login page
+      When I set email field to invalid-email@invalid-domain.com
+      And I set senha field to teste
+      And I click in button entrar
+      Then We check alert to be Email e/ou senha inválidos
 
-  Scenario: Existent User login
-    Given eu tenho um usuario que existe
-    When eu fizer uma requisicao para login
-    Then receberei status 200
-    Then receberei um token valido
+  Rule: 
+    Background:
+      Given a user with name Monkey D. Luffy, email monkeydluffy@qa.com.br and password teste exists
 
-  Scenario: Non Existent User login
-    Given eu tenho um usuario que nao existe
-    When eu fizer uma requisicao para login
-    Then receberei status 401
+    Scenario: Existent User login
+      Given I visit login page
+      When I set email field to monkeydluffy@qa.com.br
+      And I set senha field to teste
+      And I click in button entrar
+      Then I must be in /admin/home
+      And I can see Bem Vindo Monkey D. Luffy in h1
--- cypress/e2e/login/steps.js
+++ cypress/e2e/login/steps.js
@@ -1,41 +1,67 @@
 import { When, Then, Given} from "@badeball/cypress-cucumber-preprocessor";
 import {expect} from 'chai'
 
+Given(/I visit ([\w]+) page/, (page) => {
+    cy.visit(`https://front.serverest.dev/${page}`)
+})
 
-Given("eu tenho um usuario que existe", () => {
-    this.user = {
-        email: "nataliadias@qa.com.br",
-        password: "teste"
-    }
-});
+When(/I set ([\w]+) field to (.+)/, (field, value) => {
+    cy.get(`[data-testid = "${field}"]`).click().clear().type(value)
+})
 
-Given("eu tenho um usuario que nao existe", () => {
-    this.user = {
-        email: "nataliadias@qa.com.br",
-        password: "teste1"
-    }
-});
+When(/I click in button ([\w]+)/, (field) => {
+    cy.get(`[data-testid = "${field}"]`).click()
+})
 
-When("eu fizer uma requisicao para login", () => {
-    const {email, password} = this.user;
-
-    cy.request({
-        method: 'POST',
-        url: 'https://serverest.dev/login',
-        body: {
-            email: email,
-            password: password
-        },
-        failOnStatusCode: false
-    }).then((response) => {
-        this.response = response
+Then(/We check alert to be (.+)/, (label) => {
+    cy.get(`form > .alert > span`).then((field) => {
+        expect(field.text()).to.eq(label)
     })
-});
+})
 
-Then("receberei status {int}", (status) => {
-    expect(this.response.status).to.eq(status)
-});
+Given(/a user with name (.+), email (\S+@\S+\.\S+) and password (.+) exists/, (name, email, password) => {
+    cy.request("https://serverest.dev/usuarios").then((response) => {
+        const user = response.body.usuarios.find((user) => {
+            return user.email == email
+        })
 
-Then("receberei um token valido", () => {
-    expect(this.response.body.authorization).to.match(/Bearer .*/)
-});
+        const body = {
+            email: email,
+            nome: name,
+            password: password,
+            administrador: "true"
+        }
+
+        if(user) {
+            if(user.password == password && user.nome == name) return
+            cy.request({
+                method: "PUT",
+                url: `https://serverest.dev/usuarios/${user._id}`,
+                body
+            }).then((response) => {
+                expect(response.status).to.eq(200)
+            })
+        }
+
+        cy.request({
+            method: "POST",
+            url: "https://serverest.dev/usuarios",
+            body: {
+                email: email,
+                nome: "Monkey D. Luffy",
+                password: password,
+                administrador: "true"
+            }
+        }).then((response) => {
+            expect(response.status).to.eq(201)
+        })
+    })
+})
+
+Then(/I must be in (.+)/, (page) => {
+    cy.url().should('contain', page )
+})
+
+Then(/I can see (.+) in (.+)/, (value, selector) => {
+    cy.get(selector).contains(value)
+})
